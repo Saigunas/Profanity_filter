47389ed90ef4fa000a7edabec5643de444776696
Saigunas <saigunasb777@gmail.com>
2022-09-29 16:07:57 +0300

    feat: Added color change animation to a filter button

--- animations.js
+++ animations.js
@@ -3,7 +3,9 @@ const filterButtonText = filterButton.querySelector('p');
 
 filterButton.addEventListener('click', () => {
     filterButtonText.classList.add('shake-animation');
+    filterButton.classList.add('click-color-animation');
 })
 filterButtonText.addEventListener('animationend', () => {
     filterButtonText.classList.remove('shake-animation');
+    filterButton.classList.remove('click-color-animation');
 });
