f93831891ee1c23c73dea0c2b268bc6e18968847
Saigunas <saigunasb777@gmail.com>
2022-10-04 20:01:04 +0300

    feat: now includes aditions from api of top 5 words, number of words, number of sentences

--- script.js
+++ script.js
@@ -5,11 +5,24 @@ async function filterText() {
     let textToFilter = document.getElementById('text-to-filter').value;
     let replaceWith = document.getElementById('replace-with').value;
     let filteredText = await sendText(textToFilter, replaceWith);
-    displayFilteredText(filteredText);
+    displayData(filteredText);
+}
+
+class ProcessedMessage {
+    constructor(filteredText, numberOfWords, numberOfSentences, top5Words) {
+        this.filteredText = filteredText;
+        this.top5Words = top5Words;
+        this.numberOfWords = numberOfWords;
+        this.numberOfSentences = numberOfSentences;
+    }
 }
 
 async function sendText(textToFilter, replaceWith) {
     let filteredText;
+    let numberOfWords;
+    let numberOfSentences;
+    let top5Words;
+    let returnedMessage;
     try {
         res  = await fetch('https://profanity-filter-php.herokuapp.com/api/index.php', {
             method: 'POST',
@@ -21,16 +34,44 @@ async function sendText(textToFilter, replaceWith) {
                 "replace_with": replaceWith
             })
         })
-        filteredText = (await res.json()).message;
+        let returnedDocument = await res.json();
+
+        filteredText = returnedDocument.message;
+        numberOfWords = returnedDocument.number_of_words;
+        numberOfSentences = returnedDocument.number_of_sentences;
+        top5Words = returnedDocument.top_5_words;
+
+        returnedMessage = new ProcessedMessage(filteredText, numberOfWords, numberOfSentences, top5Words);
     }
     catch(err) {
         console.log(err.message);
     }
-    console.log(filteredText);
-    return filteredText;
+    return returnedMessage;
 }
 
-function displayFilteredText(filteredText) {
+function displayData(returnedMessage) {
+    document.querySelector('.extra-info').classList.remove('disabled');
+    
     let outputText = document.querySelector('.output-text');
-    outputText.textContent = filteredText;
+    let numberOfWords = document.querySelector('.number-of-words');
+    let numberOfSentences = document.querySelector('.number-of-sentences');
+    let top5Words = document.querySelector('.top-5-words');
+    
+    top5Words.innerHTML = '';
+
+    outputText.textContent = returnedMessage.filteredText;
+    numberOfWords.textContent = returnedMessage.numberOfWords;
+    numberOfSentences.textContent = returnedMessage.numberOfSentences;
+
+    let entries = returnedMessage.top5Words;
+
+    for (const key in entries) {
+        let word = key;
+        let numberOfTimes = entries[key];
+        let wordContainer = document.createElement("div");
+        wordContainer.classList.add(".top-word");
+
+        wordContainer.textContent = `\"${word}\", Number of appearances: ${numberOfTimes}`;
+        top5Words.appendChild(wordContainer);
+    }
 }
